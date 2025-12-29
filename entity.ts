6444b0d70679bf108e734651b52cb06446775ac3
Pratik Zajam <pratikzajam@medianv.com>
2025-12-29 19:39:19 +0530

    added type orm topics

---
+++ entity.ts
@@ -0,0 +1,22 @@
+// Entity in typeOrm
+
+
+
+
+// @Entity('user')
+// export class User {
+//     @PrimaryGeneratedColumn()
+//     id: number;
+
+//     @Column()
+//     name: string;
+
+//     @Column({ unique: true })
+//     email: string;
+
+//     @Column({ unique: true })
+//     isActive: boolean;
+
+//     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+//     createdAt: Date;
+// }
